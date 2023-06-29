bcc739d702912c7676c6239f26732d9e99bbaa3c
Kennedy Parks <kennedyjparks@gmail.com>
2023-06-29 14:19:46 -0700

    Added config.js

--- lifetracker-api/config.test.js
+++ lifetracker-api/config.test.js
@@ -1,27 +1,33 @@
-// require("dotenv").config()
-// require("colors")
-
+//Enables loading environment variables from our .env file
+require("dotenv").config();
+//Enables adding colors to our console.log
+require("colors");
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3001;
-const NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV : "test";
-const IS_TESTING = NODE_ENV === "test";
-
-// module.exports = { PORT, NODE_ENV };
-
+const IS_TESTING = process.env.NODE_ENV == "test" ? true : false;
+// Use dev database, testing database, or via env var, production database
+function getDatabaseUri() {
+  const dbUser = process.env.DATABASE_USER || "postgres";
+  const dbPass = process.env.DATABASE_PASS
+    ? encodeURI(process.env.DATABASE_PASS)
+    : "postgres";
+  const dbHost = process.env.DATABASE_HOST || "local";
+  const dbPort = process.env.DATABASE_PORT || 5432;
+  const dbTestName = process.env.DATABASE_TEST_NAME || "lifetracker_test";
+  const dbProdName = process.env.DATABASE_NAME || "lifetracker";
+  const dbName = process.env.NODE_ENV === "test" ? dbTestName : dbProdName;
+  return (
+    process.env.DATABASE_URL ||
+    `postgresql://${dbUser}:${dbPass}@${dbHost}:${dbPort}/${dbName}`
+  );
+}
+const BCRYPT_WORK_FACTOR = IS_TESTING ? 1 : 13;
+console.log("Lifetracker Config:".red);
+console.log("PORT:".blue, PORT);
+console.log("Database URI:".blue, getDatabaseUri());
+console.log("------".yellow);
 module.exports = {
   PORT,
-  NODE_ENV,
-  IS_TESTING
-};
-
-
-
-/*
-// require("dotenv").config()
-// require("colors")
-
-// const PORT = process.env.PORT ? Number(process.env.PORT): 3001
-// const NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV : "test"
-
-// module.exports = {PORT, NODE_ENV
-// }
-*/
+  getDatabaseUri,
+  BCRYPT_WORK_FACTOR,
+  IS_TESTING,
+}; 
